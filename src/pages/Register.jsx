d8244b9f3d159c21f5278db434130b503224b921
Orton Bright <ortonb110@gmail.com>
2023-06-17 17:44:59 +0000

    Display Alert

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,64 +3,79 @@ import { Logo, FormRow, Alert } from "../Components/index";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { useAppContext } from "../context/appContext";
 
-
 export const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  
 };
 
 const Register = () => {
-  const [values, setValues] = useState(initialState)
+  const [values, setValues] = useState(initialState);
   //Global state and useNavigate
 
-const {isLoading, showAlert} = useAppContext()
+  const { isLoading, showAlert, displayAlert } = useAppContext();
 
   const toggleMember = () => {
-    setValues({...values, isMember:!values.isMember})
-  }
+    setValues({ ...values, isMember: !values.isMember });
+  };
 
   const handleChange = (e) => {
-    console.log(e.target);
-  }
+    setValues({...values, [e.target.name]:e.target.value})
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target)
-  }
+    const {name, email, password, isMember} = values;
+    if(!name || !password || !email || (!isMember && !name)) {
+      displayAlert();
+      return
+    }
+    
+  };
 
   return (
     <Wrapper className="full-page">
-        <form className="form" onSubmit={onSubmit}>
-            <Logo/>
-            <h3>{values.isMember? 'Login': 'Register'}</h3>
-            {/* Alert Component goes here */}
-            {
-              showAlert && <Alert/>
-            }
-            {/* Name field */}
-            {
-              !values.isMember && (<FormRow type={'text'} name={'name'} value={values.name} handleChange={handleChange}/>)
-            }
-            {/* Email field */}
-            <FormRow type={'email'} name={'email'} value={values.email} handleChange={handleChange}/>
-            {/* Password field */}
-            <FormRow type={'password'} name={'password'} value={values.password} handleChange={handleChange}/>
-            <button className="btn btn-block" type="submit">submit</button> 
-            <p>
-              {values.isMember? 'Not a member yet?': 'Already a member?'}
-              <button type="button" onClick={toggleMember} className="member-btn">{
-                values.isMember? 'Register': 'Login'
-              }</button>
-            </p>
-        </form>
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {/* Alert Component goes here */}
+        {showAlert && <Alert />}
+        {/* Name field */}
+        {!values.isMember && (
+          <FormRow
+            type={"text"}
+            name={"name"}
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        {/* Email field */}
+        <FormRow
+          type={"email"}
+          name={"email"}
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* Password field */}
+        <FormRow
+          type={"password"}
+          name={"password"}
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button className="btn btn-block" type="submit">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
     </Wrapper>
   );
 };
 
 export default Register;
-
-
-
