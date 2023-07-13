75c0b06b8523ad607ac7dcf09d448d2ee30cf169
Orton Bright <ortonb110@gmail.com>
2023-07-13 22:53:07 +0000

    Working on Navigation Component

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../assets/wrappers/Navbar"
+
+const NavBar = () => {
+  return (
+    <Wrapper>
+        <h1>Hello</h1>
+    </Wrapper>
+  )
+}
+
+export default NavBar
---
+++ src/Components/SmallSideBar.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../assets/wrappers/SmallSidebar"
+
+const SmallSideBar = () => {
+  return (
+    <Wrapper>
+        <h1>Small side bar</h1>
+    </Wrapper>
+  )
+}
+
+export default SmallSideBar
