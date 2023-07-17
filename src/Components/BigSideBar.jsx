f0d02217c9881fc2bc0993d00f5786bdefcbd95f
Orton Bright <ortonb110@gmail.com>
2023-07-17 23:39:03 +0000

    Big sidebar complete

--- src/Components/BigSideBar.jsx
+++ src/Components/BigSideBar.jsx
@@ -1,9 +1,19 @@
 import Wrapper from "../assets/wrappers/BigSidebar"
-
+import NavLinks from "./NavLinks"
+import { useAppContext } from "../context/appContext"
+import Logo from "./Logo"
 const BigSideBar = () => {
+   const {showSidebar} = useAppContext()
   return (
     <Wrapper>
-        <h4>Big sidebar</h4>
+        <div className={showSidebar? 'sidebar-container ' :" sidebar-container show-sidebar"}>
+          <div className="content">
+            <header>
+              <Logo/>
+            </header>
+            <NavLinks/>
+          </div>
+        </div>
     </Wrapper>
   )
 }
