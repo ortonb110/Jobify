1efd52a77addd025a6a9d86bc0cacadbfdce88ac
Orton Bright <ortonb110@gmail.com>
2023-08-09 22:28:07 +0000

    Populate database

--- populate.js
+++ populate.js
@@ -5,20 +5,19 @@ dotenv.config();
 import Job from "./models/Job.js";
 import connectDB from "./db/connect.js";
 
-
-
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL);
-        await Job.deleteMany();
-        const jsonProduct = JSON.parse(await readFile(new URL('./Mock-data.json', import.meta.url)))
-        await Job.create(jsonProduct)
-        console.log('Success');
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URL);
+    await Job.deleteMany();
+    const jsonProduct = JSON.parse(
+      await readFile(new URL("./Mock-data.json", import.meta.url))
+    );
+    await Job.create(jsonProduct);
+    console.log("Success");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-start()
+start();
