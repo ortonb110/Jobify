8c45e47c472c7b12310499d938c1113537c9335f
Orton Bright <ortonb110@gmail.com>
2023-08-13 23:23:15 +0000

    Search filter and sort backend

--- client/src/Components/AreaChart.jsx
+++ client/src/Components/AreaChart.jsx
@@ -1,17 +1,30 @@
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts'
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-const AreaChartComponent = ({data}) => {
+const AreaChartComponent = ({ data }) => {
   return (
-    <ResponsiveContainer width={'100%'} height={300}>
-    <AreaChart data={data} margin={{top:50}}>
-      <CartesianGrid strokeDasharray={'3 3'}/>
-      <XAxis dataKey={'date'}/>
-      <YAxis allowDecimals={false}/>
-      <Tooltip/>
-      <Area dataKey={'count'} stroke='#2cb1bc' fill='#bef8fd' type={'monotone'}/>
-    </AreaChart>
-  </ResponsiveContainer>
-  )
-}
+    <ResponsiveContainer width={"100%"} height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray={"3 3"} />
+        <XAxis dataKey={"date"} />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area
+          dataKey={"count"}
+          stroke="#2cb1bc"
+          fill="#bef8fd"
+          type={"monotone"}
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default AreaChartComponent
+export default AreaChartComponent;
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -17,7 +17,23 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const {status, jobType, sort, search} = req.query;
+
+  const queryObject = {
+    createdBy: req.user.userId
+  }
+
+  if(status !== 'all') {
+    queryObject.status = status;
+  }
+  
+  if(jobType !== 'all') {
+    queryObject.jobType = jobType;
+  }
+
+  let result = Job.find(queryObject);
+
+  const jobs = await result
   res
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
