4cea847fd368e5122f0a8a59c49d79fa51f0e104
Orton Bright <ortonb110@gmail.com>
2023-08-17 23:16:01 +0000

    Backend pagination setup

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -51,6 +51,13 @@ const getAllJobs = async (req, res) => {
   }
 
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result
   res
     .status(StatusCodes.OK)
